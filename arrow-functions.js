ab54a7efce547b81e15b9b566d9507103a0e62ab
id10tothe9 <id10tothe9@googlemail.com>
2024-01-24 20:04:44 +0000

    Add exerrcises

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,39 @@
  */
 
 // Vanilla JavaScript Function
+// function multiplyNumbers(b,c) {
+//     // multiply b and c
+//     return(b*c);
+// }
+// console.log(multiplyNumbers(5,3));
 
 // Arrow Function With Parameters
+// const multiplyNumbers = (b,c) => {
+//     // Multiply numbers
+//     return(b*c);
+// }
+// console.log(multiplyNumbers(5,3));
+
 
 // Single Line Arrow Function With Parameters
+const multiplyNumbers = (a,b) => a*b;
+console.log(multiplyNumbers(5,3))
 
 // Implicit Returns
+const sayWhat = message => console.log(message);
+sayWhat('hello');
+
+const sayHi = () => console.log('Hi');
+sayHi();
 
 // Returning Multiple Lines
+const returnMultiLine = () => { return(
+    `Hi
+this
+is
+me`
+)}
+console.log(returnMultiLine());
+
+const returnOneLine = () => (console.log('Hi There'))
+returnOneLine();
